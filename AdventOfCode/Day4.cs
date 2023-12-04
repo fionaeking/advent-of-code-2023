4685dc24744f9c8d9a8930baf3dd4802a92a73c5
fionaeking <fionaeking@gmail.com>
2023-12-04 09:04:48 +0000

    Add Day 4 Part 2

--- AdventOfCode/Day4.cs
+++ AdventOfCode/Day4.cs
@@ -4,26 +4,42 @@
     {
         public void Part1()
         {
-            double sum = 0;
-            foreach (var line in input)
-            {
-                var splitLine = line.Split(" | ");
-                var winningNums = splitLine[0].Split(": ")[1].Trim().Split(" ").Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse).ToList();
-                var myNums = splitLine[1].Trim().Split(" ").Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse).ToList();
-
-                var matchCount = myNums.Where(winningNums.Contains).Count();
-
-                if (matchCount != 0)
-                {
-                    sum += Math.Pow(2, matchCount - 1);
-                }
-            }
-            Console.WriteLine(sum);
+            var partOne = input.Select(x => Parse(x)).Where(x => x.Item2 > 0).Select(x => Math.Pow(2, x.Item2 - 1)).Sum();
+            Console.WriteLine(partOne);
         }
 
         public void Part2()
         {
-            throw new NotImplementedException();
+            var scratchcardCount = 0;
+            var parsedInput = input.Select(x => Parse(x)).ToDictionary();
+
+            var cardNumsQueue = new Queue<int>(parsedInput.Keys);
+
+            while (cardNumsQueue.Count != 0)
+            {
+                scratchcardCount++;
+                var currCard = cardNumsQueue.Dequeue();
+                var matchCount = parsedInput[currCard];
+                if (matchCount != 0)
+                {
+                    for (int i = 1; i <= matchCount; i++)
+                    {
+                        cardNumsQueue.Enqueue(currCard + i);
+                    }
+                }
+            }
+
+            Console.WriteLine(scratchcardCount);
+        }
+
+        private static (int, int) Parse(string line)
+        {
+            var splitLine = line.Split(" | ");
+            var cardNum = int.Parse(splitLine[0].Split(": ")[0].Replace("Card ", ""));
+            var winningNums = splitLine[0].Split(": ")[1].Trim().Split(" ").Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse).ToList();
+            var myNums = splitLine[1].Trim().Split(" ").Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse).ToList();
+            var matchCount = myNums.Where(winningNums.Contains).Count();
+            return (cardNum, matchCount);
         }
     }
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -7,7 +7,7 @@
             var input = File.ReadAllLines("PuzzleInput.txt");
             var day = new Day4(input);
             day.Part1();
-            //sday.Part2();
+            day.Part2();
         }
     }
 }
